09e4d2d3bde7b7a71b501e0227eedc1b63e3b32b
Nova Mei <mwx6996@gmail.com>
2024-12-17 09:12:24 +0700

    chore: use next app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@repo/ui/button'
 
 export default function Home() {
-  return <Button appName="web">Open alert</Button>
+  return <Button appName="web">Open alert web</Button>
 }
